2a3327916965c3d95b74eb5e1e537bf4f56c37cd
Chris Wilson <chris+github@qwirx.com>
2013-07-30 15:01:24 +0100

    Actually add the missing hidden template row.
    Was missing from the last commit.

--- forms.py
+++ forms.py
@@ -1,9 +1,22 @@
 from django import forms
-from django.forms.models import inlineformset_factory
+from django.forms.models import inlineformset_factory, BaseInlineFormSet
+from django.utils.functional import cached_property
 
 from models import Output, Indicator, SubIndicator, Target
 
-IndicatorFormSet = inlineformset_factory(Output, Indicator, extra=1)
+class BaseIndicatorFormSet(BaseInlineFormSet):
+    @cached_property
+    def forms(self):
+        forms = super(BaseIndicatorFormSet, self).forms
+        for form in forms:
+            form.empty = False
+        empty = self.empty_form # for use by javascript
+        empty.empty = True
+        forms.append(empty)
+        return forms
+
+IndicatorFormSet = inlineformset_factory(Output, Indicator, extra=1,
+    formset=BaseIndicatorFormSet)
 TargetFormSet = inlineformset_factory(SubIndicator, Target, extra=0)
 
 class OutputForm(forms.ModelForm):
