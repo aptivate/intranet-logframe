273322716a482df951987ce5721c2601e4fd3147
Hamish Downer <hamish@aptivate.org>
2013-10-15 15:32:30 +0100

    Output assumptions and risk rating go in and out of db
    previously were hard wired in the template

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import LogFrame, Milestone
+from .models import LogFrame, Milestone, RiskRating
 
 admin.site.register(LogFrame)
 admin.site.register(Milestone)
+admin.site.register(RiskRating)
