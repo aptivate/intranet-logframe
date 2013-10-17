40f7b682fa6381ac241be722cecd4d77f3201f59
Hamish Downer <hamish@aptivate.org>
2013-10-17 12:01:31 +0100

    Set the background color of description according to status
    so red-ish, yellow-ish or green-ish depending on whether the status is
    danger, warning or ok

--- static/js/logframe.js
+++ static/js/logframe.js
@@ -10,5 +10,13 @@
 		node.style.background= "linear-gradient(to right,"+barFill+" "+percent+"%, "+barEmpty+" "+percent+"%)" ;
 	}
 
+	function setBackgroundByStatus(index, node) {
+		var statusToColor = {'danger': "#e68080", 'warning': "#ffcc66", 'ok': "#85ce9d"};
+		if (node.dataset.status in statusToColor) {
+			node.style.background = statusToColor[node.dataset.status];
+		}
+	}
+
 	$(".barchart-background").each(backgroundBarChart);
+	$(".status-background").each(setBackgroundByStatus);
 })();
